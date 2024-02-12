cff5a56ebf65eebadc410d9cb632ce0a1476c68a
ratulhasan001 <ratulhasan2108@gmail.com>
2024-02-13 00:44:10 +0600

    updated

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-let bt = document.getElementById("blog");
-bt.addEventListener("click", () => {
-    console.log("Worked");
-});
+
+
+const loadAllData = () => {
+    fetch("https://openapi.programming-hero.com/api/videos/category/1000")
+   .then((res) => res.json())
+   .then((data) => displayData(data.data))
+};
+
+const displayData = (data) => {
+    console.log(data);
+    const alldata = document.getElementById("printdata");
+    data.forEach((dt) => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <img src="${dt.thumbnail}" alt="">
+        <h2>${dt?.others.views}</h2>
+        `;
+        alldata.appendChild(card);
+    }) 
+};
