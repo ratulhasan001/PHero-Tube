c114fdc17129a74e2b4ad1815f95dd4984c0a753
ratulhasan001 <ratulhasan2108@gmail.com>
2024-02-14 00:56:58 +0600

    api completed

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ let all = false;
 let music = false;
 let comedy = false;
 let drawing = false;
+let toggle_all = false;
+let toggle_music = false;
+let toggle_comedy = false;
+let toggle_drawing = false;
 let cnt = 0;
+
 document.getElementById("all_btn").addEventListener("click", function(e) {
   all = true;
   music = false;
@@ -46,25 +51,48 @@ const loadcomedydata = () => {
     .then((data) => displayData(data.data));
 };
 const loadnoData = () => {
-  fetch("https://openapi.programming-hero.com/api/videos/category/1002") // ${id}
+  fetch("https://openapi.programming-hero.com/api/videos/category/1002")
     .then((res) => res.json())
     .then((data) => displayData(data.data));
 };
 
 const displayData = (data) => {
   console.log(data);
+  if(data.length == 0) {
+    const alldata = document.getElementById("printdata");
+    const card = document.createElement("div");
+    while(printdata.firstElementChild) printdata.removeChild(printdata.firstElementChild);
+    card.classList.add("aladabox");
+    card.innerHTML = `
+    <div class="noContentimg">
+      <div id = "contentimg">
+      <img src="./img/Icon.png" alt="">
+      </div>
+      <h1 style = "margin-top: 20px;font-weight: bold">Oops!! Sorry,</h1>
+      <h1 style = "font-weight : bold">There is no content here</h1>
+    </div>
+  </div>
+    `
+    alldata.appendChild(card);
+    return;
+  }
   const alldata = document.getElementById("printdata");
   while(printdata.firstElementChild) printdata.removeChild(printdata.firstElementChild);
   data.forEach((dt) => {
     const card = document.createElement("div");
     let ver = false;
     if(dt.authors[0].verified == true) ver = true;
+    let second = dt.others.posted_date;
+    
+    let hours = Math.floor(second / 3600);
+    let min = Math.floor((second % 3600) / 60);
+    
     card.classList.add("box");
     card.innerHTML = `
     <div class="imgdiv">
       <img style = "border-radius: 10px; width:350px ;height : 200px;" class
       "imgbox" src="${dt. thumbnail}" alt="s">
-      <br />
+      <br/>
       <div class="d-flex">
         <div>
         <img class="author" src="${dt.authors[0].profile_picture}" alt="" />
@@ -73,6 +101,7 @@ const displayData = (data) => {
         <p class = "p-info" style = "font-size : 20px; font-weight: bold;">${dt.title}</p>
         <p class = "p-info" style = "opacity : 60%">${dt.authors[0].profile_name}        ${(dt.authors[0].verified == true) ? `<i class="fa-solid fa-circle-check" style="color: #001eff;"></i>` : ''}</p>
         <p class = "p-info" style = "opacity : 60%">${dt.others.views}</p>
+        <p id = "time">${hours != 0 && min != 0 ? `<p id = "time2">${hours} hours ${min} minutes</p>` : ''}</p>
         </div>
       </div>
     </div>
@@ -116,15 +145,44 @@ const sortbyviewD = (categoryId) => {
 document.getElementById("sortbyview").addEventListener("click", function(e) {
 
   if(all == true) {
-    sortbyview(1000);
+    if(!toggle_all) {
+      sortbyview(1000);
+      toggle_all = true;
+    }
+    else if(toggle_all) {
+      sortbyviewD(1000);
+      toggle_all = false;
+    }
+    
   }
   else if(music == true) {
-    sortbyview(1001);
+    if(!toggle_music) {
+      sortbyview(1001);
+      toggle_music = true;
+    }
+    else if(toggle_music) {
+      sortbyviewD(1001);
+      toggle_music = false;
+    }
   }
   else if(comedy == true) {
-    sortbyview(1003);
+    if(!toggle_comedy) {
+      sortbyview(1003);
+      toggle_comedy = true;
+    }
+    else if(toggle_comedy) {
+      sortbyviewD(1003);
+      toggle_comedy = false;
+    }
   }
   else {
-    sortbyview(1002);
+    if(!toggle_music) {
+      sortbyview(1001);
+      toggle_music = true;
+    }
+    else if(toggle_music) {
+      sortbyviewD(1001);
+      toggle_music = false;
+    }
   }
 }) 
